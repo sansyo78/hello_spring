db0ae8f47597eb2554f9f9cb4a441910cbd9bf73
sansyo <sansyo78@gmail.com>
2022-04-02 16:17:34 +0900

    add service

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,12 +1,14 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository{
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence =0L;
+    private static long sequence = 0L;
 
     @Override
     public Member save(Member member) {
@@ -22,7 +24,7 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-       return store.values().stream()
+        return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
@@ -31,4 +33,10 @@ public class MemoryMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+
+    public void clearStore() {
+        store.clear();
+    }
+
 }
